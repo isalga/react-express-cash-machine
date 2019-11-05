fe095f163b1c885d01895212861a27cbecb6e9f1
Iris Salcedo <irissalcedo@protonmail.com>
2019-11-05 12:56:59 +0100

    Update client test&remove generated CSS

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -12,9 +12,12 @@ it('first load renders correctly', () => {
 
 it('renders correctly when submit', () => {
   const component = renderer.create(<App />);
+  const inputText = component.root.find(el => el.type === 'input' && el.props.type === 'text');
   const submitBtn = component.root.find(el => el.type === 'input' && el.props.type === 'submit');
 
   act(() => {
+    inputText.value = '10';
+    ReactTestUtils.Simulate.change(inputText);
     ReactTestUtils.Simulate.click(submitBtn);
   });
 
